87e319909853d55a9d194f5ee6c8df627b5ee687
Alex Paul <al.9thousand@gmail.com>
2020-01-26 00:54:51 -0500

    server works

--- app.js/index.js
+++ app.js/index.js
@@ -3,19 +3,37 @@ const path = require('path')
 const volleyball = require('volleyball')
 
 const app = express()
+const port = 3000
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('*', (req, res, next) => {
-  try {
-    res.sendFile()
-  } catch (error) {
-    next(error)
-  }
-})
+
+
+// app.use('*', (req, res, next) => {
+//   try {
+//     res.sendFile()
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
 
 app.use((err, req, res, next) => {
   console.log(err.stack)
   res.status(err.status || 500).send(err.message || 'Internal server error')
 })
+
+app.get('/', (req, res, next) => {
+  try {
+    res.status(200).send('Hello World!')
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+
+
+app.listen(port, () => console.log(`Soundboard ready on port ${port}!`))
+
+module.exports = app
